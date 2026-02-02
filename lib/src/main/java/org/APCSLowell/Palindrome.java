dd9d034b12a2b1250898c47550077bf68c2d2233
kevin12132525 <kevin@andrew-gordon.com>
2026-02-02 04:37:04 +0000

    happy_febuary

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,14 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        return s.equals(reverse(s));
     }
     public String reverse(String s) {
         String n = new String();
-        // your code here
+        for(int i = s.length(); i >0; i--){
+            n += s.substring(i-1,i);
+        }
         return n;
     }
 }
